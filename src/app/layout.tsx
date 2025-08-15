3f7609754abb42078b5664cc2072c1fedf1579c0
QuynhXEM <quynh@xem.edu.vn>
2025-08-15 12:25:17 +0700

    Fix Duplicate Metadata

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,33 +1,15 @@
 import "./globals.css";
 import "react-tooltip/dist/react-tooltip.css";
 import { chakraPetch } from "@/assets/font";
-import { fetchAppMetadata } from "@/libs/utils";
-import { cookies } from "next/headers";
 import { Analytics } from "@vercel/analytics/next";
 import Script from "next/script";
-import { GATracker } from "@/views/GATracker";
-
 export default async function RootLayout({
   children,
 }: {
   children: React.ReactNode;
 }) {
-  const cookieStore = await cookies();
-  let locale = cookieStore.get("NEXT_LOCALE")?.value;
-  const metadata = await fetchAppMetadata(locale);
-  const iconId = metadata?.icon;
-  const faviconUrl = `${process.env.NEXT_PUBLIC_API_URL}/assets/${iconId}/ids-coin.svg?v=${Date.now()}`;
-
   return (
     <html lang="en" suppressHydrationWarning>
-      <head>
-        <link rel="icon" href={faviconUrl} />
-        <meta
-          name="description"
-          content={metadata?.translation?.[0]?.description}
-        />
-        <title>{metadata?.translation?.[0]?.name}</title>
-      </head>
 
       {/* GA Scripts */}
       <Script
