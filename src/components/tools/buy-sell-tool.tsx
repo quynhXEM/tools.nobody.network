519c0298901abea849d582288a0b41ccbadde54d
QuynhXEM <quynh@xem.edu.vn>
2025-09-11 16:37:44 +0700

    Moonpay Buy Crypto

--- src/components/tools/buy-sell-tool.tsx
+++ src/components/tools/buy-sell-tool.tsx
@@ -16,20 +16,20 @@ const MoonPayBuyWidget = dynamic(
 export const BuySellCryptoTool = () => {
     return <MoonPayProvider apiKey={process.env.NEXT_PUBLIC_MOONPAY_API_KEY} debug>
 
-        <div className="w-full max-w-md mx-auto space-y-4">
-            <Card className="bg-slate-800/50 backdrop-blur-xl border-slate-700/50 shadow-2xl">
+        <div className="w-full max-w-lg mx-auto space-y-4">
+            <Card className="bg-slate-800/50 backdrop-blur-xl shadow-2xl">
                 <CardHeader className="">
                     <div className="flex items-center justify-between">
                         <CardTitle className="text-xl font-semibold text-white flex items-center gap-2">
-                            <BuySellCryptoIcon color='cyan' className={''}/>
+                            <BuySellCryptoIcon color='cyan' className={''} />
                             Buy/Sell Crypto
                         </CardTitle>
                     </div>
                 </CardHeader>
 
-                <CardContent>
+                <CardContent className='border-none'>
                     <MoonPayBuyWidget
-                        style={{ width: '100%', backgroundColor: "transparent", margin: 0 }}
+                        style={{ width: '100%', backgroundColor: "transparent", margin: 0, border: "none" }}
                         variant="embedded"
                         baseCurrencyCode="usd"
                         defaultCurrencyCode="eth"
